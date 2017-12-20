500562dba754ac97a5c67e2f9910e85132c463b7
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-12-20 17:42:38 +0800

    add test

---
+++ machinelearningDLWJ/src/main/java/SingleLayerNeuralNetworks/Perceptrons.java
@@ -0,0 +1,9 @@
+package SingleLayerNeuralNetworks;
+public class Perceptrons {
+    final int train_N=1000; //训练数据的数量
+    final int test_N=200;
+    final int nIn = 2;
+
+    double[][] train_X = new double[train_N][nIn];
+
+}
